ec9d8064ad82a63237bc403bd70c3ea31bde5847
João Guilherme Da Rocha <joaoguibc@gmail.com>
2024-05-22 18:22:53 +0000

    API setup

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,38 @@
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { routes } from './routes'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'SaaS',
+      description: 'Full-stack SaaS app with multi-tennant & RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+app.register(fastifyCors)
+
+app.register(routes)
+
+app.listen({ port: 3333 }).then(() => console.log(`App running on port 3333`))
